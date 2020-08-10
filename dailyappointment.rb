ed83c6f3fbc3a68003e694a83d176deb00eab553
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-10 01:14:19 -0400

    Se crea la clase de herencia y desarrolla programa

---
+++ dailyappointment.rb
@@ -0,0 +1,9 @@
+class DailyAppointment < Appointment #Creación de clase de herencia 
+    def occurs_on?(hour,min)
+        @hour == hour && @min == min
+    end
+
+    def to_s
+        "Reunión diaria en #{location} sobre #{purpose} a las #{hour}: #{min}"
+    end
+end
